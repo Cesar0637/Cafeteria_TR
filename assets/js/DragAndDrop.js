389e83689aed39a093d08016fa94c98a763d84f8
Cesar0637 <prospartan0637@gmail.com>
2024-09-13 12:36:26 -0600

    API Google + Drag And Drop

---
+++ assets/js/DragAndDrop.js
@@ -0,0 +1,74 @@
+﻿//Los evento de JS son acciones o suscesos que ocurren en un docuemnto web y que pueden ser detectados y manejados por le código JS. Estos eventos puedes ser desencadenados por el usuario o generados automáticamente por le navegador
+
+//En t+erminos más especificos, un evento es algo que sucede en el sistema que estámos programando. El sistema produce un señal de cierto tipo cuando en evento ocurre y proporciona un mecanismo para que una acción se lleve a cabo automáticamente como respuesta a dicho evento
+
+var  contador = 0
+var elemArrastrable = ""
+var contadorA = 0
+
+function start(e) {
+    //Funcion que se eja=ecuta con el evento 'ondragstart', recibe como parametro el event propio
+
+    console.log("start")
+    e.dataTransfer.effectAllowed = "move"//el movimiento del elemento
+    e.dataTransfer.setData("Data", e.target.id)//guarda en cache la info del elemento
+    $("#" + e.target.id).css("opacity", "0.4")//cambia el css 
+    console.log(e.target.id)
+    elemArrastrable = e.target.id
+
+}
+
+function end(e) {
+    console.log("end")
+    e.target.style.opacity = ''//restablece la propiedad de opacidad a su valor por defecto
+    e.dataTransfer.clearData("Data") //lipia cache 
+    elemArrastrable = ""
+    console.log(e.target)
+}
+function enter(e) {
+    console.log("enter")
+    e.target.style.border = "12px dotted #555"
+}
+function leave(e) {
+    console.log("leave");
+    $("#" + e.target.id).css("border", "")
+}
+function over(e) {
+    console.log("Over")
+    var id = e.target.id
+    if ((id == "cuadro1") || (id == "cuadro3") || (id == "papelera")) {
+        return false
+    } else { 
+        return true
+    }
+}
+
+function drop(e) {
+    console.log("drop");
+    var elementoArrastrado = e.dataTransfer.getData("Data")
+    e.target.appendChild(document.getElementById(elementoArrastrado));
+    e.target.style.border = ""
+}
+
+function remove(e) {
+    console.log("delete")
+    var elementoArrastrado = document.getElementById(e.dataTransfer.getData("Data"))
+    elementoArrastrado.parentNode.removeChild(elementoArrastrado)
+    e.target.style.border = ""
+    contadorA --
+}
+
+function clone(e) {
+    console.log("clone")
+    var elementoArrastrado = document.getElementById(e.dataTransfer.getData("Data"))
+    elementoArrastrado.style.opacity = ""
+    if (contadorA < 3) {
+        var elementoClonado = elementoArrastrado.cloneNode(true)
+        elementoClonado.id = "ClonedNode" + contador
+        contador++
+        contadorA++
+        elementoClonado.style.position = "static"
+        e.target.appendChild(elementoClonado)
+    }
+    e.target.style.border = ""
+}
--- assets/js/MiScript.js
+++ assets/js/MiScript.js
@@ -55,7 +55,7 @@ function ValidarHora() {
             posible = true
         }
     } else {
-        
+
         if (timecontrol.value > apertura && timecontrol.value < cierre) {
             if (timecontrol.value < formatted) {
                 icono = "warning"
@@ -63,7 +63,7 @@ function ValidarHora() {
                 msg = "Una reservacion debe tener 2 horas de antelacion"
                 posible = false
             } else {
-            posible = true
+                posible = true
             }
         } else {
             icono = "warning"
@@ -115,7 +115,7 @@ function Sumar() {
     var a = parseInt(personas.value)
     var b = parseInt(personas_add.value)
 
-    if (a < 0 || b < 0) {
+    if (a <= 0 || b < 0) {
         posible = false
         icono = "warning"
         titulo = "Algo Salio Mal"
@@ -141,7 +141,7 @@ function validar() {
     var a = parseInt(personas.value)
     var b = parseInt(personas_add.value)
 
-    if (a > 8 || b > 4) {
+    if (a >= 8 || b >= 4) {
         icono = "warning"
         titulo = "Limite Alcanzado"
         msg = "La reservacion solo puede hacerse para un maximo de 12 ersonas (8 + 4)"
@@ -185,6 +185,9 @@ function getGeo() {
 
 function geoOK(position) {
     console.log(position)
+    showLatLong(position.coords.latitude, position.coords.longitude)
+    initMap2(position.coords.latitude, position.coords.longitude)
+
 }
 function geoERROR(error) {
     if (error.code == 1) {
@@ -201,4 +204,69 @@ function geoERROR(error) {
         alert("Error: " + error.code)
     }
 
+}
+
+function showLatLong(lat, long) {
+   
+    //para el street view
+    var geocoder = new google.maps.Geocoder();
+    var milocalizacion = new google.maps.LatLng(lat, long)
+    console.log(milocalizacion)
+    geocoder.geocode({ 'latLng': milocalizacion }, processGeocoder)
+}
+
+
+function processGeocoder(result, status) {
+    console.log(result)
+    console.log(status)
+    if (status == google.maps.GeocoderStatus.OK) {
+        //esperamos los resultados de google para obtener una direccion real en lugar de solo cordenadas
+        if (result[0]) {
+            var direccion = result[0].formatted_addres
+            //buscamos (usandoJquery) el elemento direccion y colocamos la direccion que nos respondio google
+            $("direccion").html(direccion)
+        }
+        else {
+            error("Google no retorno ningun resultado")
+        }
+    }
+    else {
+        error("Google marco un Error")
+    }
+}
+
+let map;
+
+async function initMap() {
+
+    const { map } = await google.maps.importLibrary("maps")
+}
+
+function initMap2(lat, lng) {
+    //genero la informacion para obtener un mapa desde Google
+    var miscoordenadas = new google.maps.LatLng(lat, lng)
+    //opciones para el mapa
+
+    var mapoptions = {
+        zoom: 15 ,
+        center: miscoordenadas,
+        mapTypedId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(document.getElementById("map"), mapoptions)
+    //Configuro un marcador de osicion para mi mapa
+    new google.maps.Marker({
+        position: miscoordenadas,
+        map,
+        title: " Hello World!"
+    })
+
+    $('#street').css("height", 300)
+    //creo y configuro el streetview
+    var panorama = new google.maps.StreetViewPanorama(document.getElementById("street"),
+        { position: miscoordenadas, setpov: { heading: 90, pitch: 5 }})
+    //mapa en street view
+    map.setStreetView(panorama)
+    //recargo el mapa por ultima vez
+    window.initMap = initMap;
 }
